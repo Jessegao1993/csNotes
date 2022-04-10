38c1ccaf4f017d4296f98752f2f61efc68956392
JesseGaoMac <gaoqijesse@gmail.com>
2022-04-10 23:01:37 +0800

    20220410

--- ts/webpack/webpack.config.js
+++ ts/webpack/webpack.config.js
@@ -2,6 +2,8 @@
 const path = require('path')
 //引入html插件
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+//引入clean插件
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 module.exports = {
     //指定入口文件
     entry:"./src/index.ts",
@@ -9,14 +11,32 @@ module.exports = {
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:"bundle.js",
-
+        environment:{
+            arrowFunction:false
+        }
     },
     //指定webpack打包时要使用的模块
     module:{
         rules:[
             {
                 test:/\.ts$/,
-                use:'ts-loader',
+                use:[{
+                    loader:"babel-loader",
+                    options:{
+                        presets:[
+                             [
+                                 "@babel/preset-env",
+                                 {
+                                     targets:{
+                                         "chrome":"88"
+                                     },
+                                     "corejs":"3",
+                                     "useBuiltIns":"usage" 
+                                 }
+                             ]
+                        ]
+                    }
+                },'ts-loader'],
                 exclude:'/node-modules/'
             }
         ]
@@ -26,5 +46,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             title:"This is homepage"
         }),
-    ]
+        new CleanWebpackPlugin()
+    ],
+
+    //设置引用模块
+    resolve:{
+        extensions:['.ts','.js']
+    }
 }
